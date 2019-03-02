1685557e6f1db567024858f9c1f22e5b730ff4c8
Yuma Ohgami <yuma@ohgami.jp>
2019-03-02 15:12:41 +0900

    add 013_set_command.

---
+++ 013_set_command/main.c
@@ -0,0 +1,91 @@
+#include <x86.h>
+#include <intr.h>
+#include <pic.h>
+#include <acpi.h>
+#include <fb.h>
+#include <kbc.h>
+#include <fbcon.h>
+#include <fs.h>
+#include <hpet.h>
+#include <sched.h>
+#include <common.h>
+#include <syscall.h>
+#include <proc.h>
+#include <pci.h>
+#include <nic.h>
+
+struct __attribute__((packed)) platform_info {
+	struct framebuffer fb;
+	void *rsdp;
+};
+
+#define INIT_APP	"test"
+
+void start_kernel(void *_t __attribute__((unused)), struct platform_info *pi,
+		  void *_fs_start)
+{
+	/* フレームバッファ周りの初期化 */
+	fb_init(&pi->fb);
+	set_fg(255, 255, 255);
+	set_bg(0, 70, 250);
+	clear_screen();
+
+	/* ACPIの初期化 */
+	acpi_init(pi->rsdp);
+
+	/* CPU周りの初期化 */
+	gdt_init();
+	intr_init();
+
+	/* 周辺ICの初期化 */
+	pic_init();
+	hpet_init();
+	kbc_init();
+
+
+
+	/* 一旦、コマンドとステータスを読み出す */
+	unsigned int conf_data = get_pci_conf_reg(
+		NIC_BUS_NUM, NIC_DEV_NUM, NIC_FUNC_NUM,
+		PCI_CONF_STATUS_COMMAND);
+
+	/* ステータス(上位16ビット)をクリア */
+	conf_data &= 0x0000ffff;
+	/* コマンドに割り込み無効設定 */
+	conf_data |= PCI_COM_INTR_DIS;
+
+	/* コマンドとステータスに書き戻す */
+	set_pci_conf_reg(NIC_BUS_NUM, NIC_DEV_NUM, NIC_FUNC_NUM,
+			 PCI_CONF_STATUS_COMMAND, conf_data);
+
+	/* PCIコンフィグレーション空間からコマンドとステータスをダンプ */
+	dump_command_status(NIC_BUS_NUM, NIC_DEV_NUM, NIC_FUNC_NUM);
+
+	/* haltして待つ */
+	while (1)
+		cpu_halt();
+
+
+
+	/* システムコールの初期化 */
+	syscall_init();
+
+	/* ファイルシステムの初期化 */
+	fs_init(_fs_start);
+
+	/* スケジューラの初期化 */
+	sched_init();
+
+	/* CPUの割り込み有効化 */
+	enable_cpu_intr();
+
+	/* スケジューラの開始 */
+	sched_start();
+
+	/* initアプリ起動 */
+	exec(open(INIT_APP));
+
+	/* haltして待つ */
+	while (1)
+		cpu_halt();
+}
